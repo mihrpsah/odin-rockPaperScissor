7a4ca4441ace29353e59b268ceafe799b221afe8
Mihir Pratap Sah <smihirp1@gmail.com>
2022-07-11 22:12:20 +0530

    v1.0 built

---
+++ script.js
@@ -0,0 +1,45 @@
+function computerPlay(){
+    let cMove = Math.floor(Math.random() * 3);
+    console.log(cMove)
+    return cMove
+}
+
+function playRound(playerSelection, computerSelection){
+    if(computerSelection == 0 && playerSelection == 1){
+        return 1;
+    }
+    else if(computerSelection == 0 && playerSelection == 0){
+        return 0;
+    }
+    else if(computerSelection == 0 && playerSelection == 2){
+        return 2;
+    }
+    else if(computerSelection == 1 && playerSelection == 0){
+        return 2;
+    }
+    else if(computerSelection == 1 && playerSelection == 1){
+        return 0;
+    }
+    else if(computerSelection == 1 && playerSelection == 2){
+        return 1;
+    }
+    else if(computerSelection == 2 && playerSelection == 0){
+        return 1;
+    }
+    else if(computerSelection == 2 && playerSelection == 1){
+        return 2;
+    }
+    else return 0;
+}
+
+let pMove  = prompt("Make your move (Rock-0, Paper-1, Scissor-2)")
+let result = playRound(pMove, computerPlay());
+
+if(result == 0){
+    console.log("Tie")
+}
+else if(result == 1){
+    console.log("You Won")
+}
+else console.log("You Lost")
+
